9b45f6889fda86536e61d664dc2ebd171b988b55
scjtqs <scjtqs@qq.com>
2021-04-24 14:51:16 +0800

    去掉 32位支持

--- buildForTdlib.sh
+++ buildForTdlib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuildertdlib
-docker buildx build --tag scjtqs/tdlib:1.7.0 --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386 --push -f Dockerfile.td .
-docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/tdlib:1.7.0 --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386 --push -f Dockerfile.td .
+docker buildx build --tag scjtqs/tdlib:1.7.0 --platform linux/amd64,linux/arm64 --push -f Dockerfile.td .
+#docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/tdlib:1.7.0 --platform linux/amd64,linux/arm64 --push -f Dockerfile.td .
 docker buildx rm mybuildertdlib
