1d5b640d61a7ad382ff0c82fdcfd959c7411bca7
scjtqs <scjtqs@qq.com>
2021-05-04 15:31:43 +0800

    rm qemu

--- buildForTdlib.sh
+++ buildForTdlib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuildertdlib
 docker buildx build --tag scjtqs/tdlib:1.7.0 --platform linux/amd64,linux/arm64,linux/386,linux/arm/v6,linux/ppc64le  --push -f Dockerfile.td .
 #docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/tdlib:1.7.0 --platform linux/amd64,linux/arm64 --push -f Dockerfile.td .
--- buildtg.sh
+++ buildtg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuildergotg
 docker buildx build --tag scjtqs/go-tg:latest --platform linux/amd64,linux/arm64,linux/386,linux/arm/v6,linux/ppc64le --push -f Dockerfile.go-td .
 docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/go-tg:latest --platform linux/amd64,linux/arm64,linux/386,linux/arm/v6,linux/ppc64le --push -f Dockerfile.go-td .
-docker buildx rm mybuildergotg
+#docker buildx rm mybuildergotg
