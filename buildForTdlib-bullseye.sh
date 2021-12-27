36c529f0c56d05d8f0b47a4746a11c3100f47d18
scjtqs <scjtqs@qq.com>
2021-12-27 09:51:14 +0800

    arm v7

--- buildForTdlib-bullseye.sh
+++ buildForTdlib-bullseye.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuildertdlib
-docker buildx build --tag scjtqs/tdlib:1.7.10-bullseye --platform linux/amd64,linux/arm64,linux/386,linux/arm/v6  --push -f bullseye.td.Dockerfile .
+docker buildx build --tag scjtqs/tdlib:1.7.10-bullseye --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7  --push -f bullseye.td.Dockerfile .
 docker buildx rm mybuildertdlib
