2516631adf8e5b294e089e7873f8efe1c180bb00
scjtqs <scjtqs@qq.com>
2024-05-23 18:08:44 +0800

    fix: 交给github编译

--- buildtg-latest.sh
+++ buildtg-latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --use --name mybuildergotg
+#docker buildx create --use --name mybuildergotg
 docker buildx build --tag scjtqs/go-tg:latest --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7 --push -f alpine.go-td.Dockerfile .
 docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/go-tg:latest --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7 --push -f alpine.go-td.Dockerfile .
-docker buildx rm mybuildergotg
+#docker buildx rm mybuildergotg
